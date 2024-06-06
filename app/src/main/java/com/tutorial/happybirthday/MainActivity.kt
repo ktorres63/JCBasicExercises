d7b85668271fb0d94085fa8c9735db262326667e
ktorres63 <ktorresaro@unsa.edu.pe>
2024-06-06 12:43:39 -0500

    Finish Quadrants exercise

--- app/src/main/java/com/tutorial/happybirthday/MainActivity.kt
+++ app/src/main/java/com/tutorial/happybirthday/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -27,8 +29,11 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.tutorial.happybirthday.ui.theme.ColumnColor
 import com.tutorial.happybirthday.ui.theme.HappyBirthdayTheme
+import com.tutorial.happybirthday.ui.theme.ImageColor
 import com.tutorial.happybirthday.ui.theme.Purple40
+import com.tutorial.happybirthday.ui.theme.RowColor
 import com.tutorial.happybirthday.ui.theme.TextColor
 
 class MainActivity : ComponentActivity() {
@@ -41,9 +46,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TaskManagerView(
-                        tasks = stringResource(id = R.string.tasks),
-                        message = stringResource(id = R.string.message)
+                    QuadrantsView(
+                        tit1 = stringResource(id = R.string.text_title),
+                        descr1 = stringResource(id = R.string.text_description),
+                        tit2 = stringResource(id = R.string.image_title),
+                        descr2 = stringResource(id = R.string.image_description),
+                        tit3 = stringResource(id = R.string.row_title),
+                        descr3 = stringResource(id = R.string.row_description),
+                        tit4 = stringResource(id = R.string.column_title),
+                        descr4 = stringResource(id = R.string.column_description)
                     )
 
                 }
@@ -158,9 +169,13 @@ fun Quadrant(
     modifier: Modifier = Modifier
 ) {
     Column(
-        Modifier
+        modifier
+            .fillMaxSize()
             .background(color)
-            .padding(16.dp)) {
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
             text = title,
             fontWeight = FontWeight.Bold,
@@ -175,11 +190,76 @@ fun Quadrant(
     }
 }
 
-fun QuadrantsView() {
-    
+@Composable
+fun QuadrantsView(
+    tit1: String,
+    descr1: String,
+    tit2: String,
+    descr2: String,
+    tit3: String,
+    descr3: String,
+    tit4: String,
+    descr4: String, modifier: Modifier = Modifier
+) {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            Quadrant(
+                title = tit1,
+                description = descr1,
+                color = TextColor,
+                modifier = Modifier.weight(1f),
+            )
+            Quadrant(
+                title = tit2,
+                description = descr2,
+                color = ImageColor,
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            Quadrant(
+                title = tit3,
+                description = descr3,
+                color = RowColor,
+                modifier = Modifier.weight(1f)
+            )
+            Quadrant(
+                title = tit4,
+                description = descr4,
+                color = ColumnColor,
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
 
 }
 
+@Preview(showBackground = true)
+@Composable
+fun ComposeQuadrant() {
+    HappyBirthdayTheme {
+        QuadrantsView(
+            tit1 = stringResource(id = R.string.text_title),
+            descr1 = stringResource(id = R.string.text_description),
+            tit2 = stringResource(id = R.string.image_title),
+            descr2 = stringResource(id = R.string.image_description),
+            tit3 = stringResource(id = R.string.row_title),
+            descr3 = stringResource(id = R.string.row_description),
+            tit4 = stringResource(id = R.string.column_title),
+            descr4 = stringResource(id = R.string.column_description),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RowQuadrants() {
+    Row (Modifier.fillMaxSize()){
+        Text(text = stringResource(id = R.string.text_description), Modifier.background(TextColor).weight(1f))
+        Text(text = stringResource(id = R.string.image_description),Modifier.background(ImageColor).weight(1f))
+
+    }
+}
 
 //@Preview(showBackground = true)
 @Composable
@@ -212,17 +292,6 @@ fun TaskManagerPreview() {
             tasks = stringResource(id = R.string.tasks),
             message = stringResource(id = R.string.message),
             modifier = Modifier.fillMaxSize()
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun ComposeQuadrant() {
-    HappyBirthdayTheme {
-        Quadrant(
-            title = stringResource(id = R.string.text_title),
-            description = stringResource(id = R.string.text_description)
         )
     }
 }
